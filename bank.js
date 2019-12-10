f5de2fc173a6e395616f1c46eb502d857aa3767a
Naresh Sonagoni <nareshsonagoni@gmail.com>
2019-12-10 23:40:10 +1100

    adding very important function.

---
+++ bank.js
@@ -0,0 +1,40 @@
+function veryveryimportantfunction() {
+    ..
+    ...IntersectionObserver.apply
+        .apply.apply.apply.apply.apply
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ss
+
+
+
+
+
+
+        .apply.apply.apply.apply.apply
+
+    bankcalucation = currentbalance + interest;
+}
