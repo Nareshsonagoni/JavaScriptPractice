bb3d1189929ef83c023ef190196c923a3b7e7fab
Naresh Sonagoni <nareshsonagoni@gmail.com>
2019-12-11 03:39:49 +1100

    Element selection in js

---
+++ main.js
@@ -0,0 +1,10 @@
+// Single Element
+
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('h1'));
+
+// Multiple Element
+
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByClassName('item'));
+console.log(document.getElementsByTagName('li'));
