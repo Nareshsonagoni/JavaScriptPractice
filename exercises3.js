5be45360d72f62c74edf0505f9e9407c6342a3c6
Naresh Sonagoni <nareshsonagoni@gmail.com>
2019-12-11 02:54:53 +1100

    class in js

--- exercises3.js
+++ exercises3.js
@@ -1,14 +1,46 @@
-function playVideo(a, b) {
-    return this, a, b;
+
+// Constructor Function
+
+function Person(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+    this.fullName = function () {
+        return `${this.firstName} ${this.lastName}`;
+    }
+    this.getBirthYear = function () {
+        return this.dob.getFullYear();
+    };
+
+    this.email = function () {
+        return `${this.firstName}.${this.lastName}@company.com`;
+    }
 }
 
-let val = playVideo.call({ name: 'Naresh' }, 1, 2);
-console.log(val);
-playVideo.apply({ name: 'kuppu' }, [3, 4]);
-playVideo.bind({ name: "isha" }, 5, 6)();
-playVideo.call({ name: 'Tarun' }, 9, 10);
-// play(7, 9);
+const naresh = new Person('Naresh', 'Sonagoni', '06-10-1989');
+
+// console.log(naresh.getBirthYear());
+
+// console.log(naresh.email());
 
 
+class People {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
 
+    fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    birthYear() {
+        return this.dob.getFullYear();
+    }
+}
+
+const peep = new People('Isha', 'Sonagoni', '24-02-2019');
+
+console.log(peep.fullName());
 
